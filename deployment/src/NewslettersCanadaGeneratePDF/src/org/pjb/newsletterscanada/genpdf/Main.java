6574d25b0f385c8b29b716624893699a609f9785
PJ Boirleaud <boirleaud@gmail.com>
2016-11-02 23:24:47 -0400

    Added source code for deployment scripts

---
+++ deployment/src/NewslettersCanadaGeneratePDF/src/org/pjb/newsletterscanada/genpdf/Main.java
@@ -0,0 +1,79 @@
+package org.pjb.newsletterscanada.genpdf;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public final static String PAGES_PATH = "../website/canada/";
+	public final static String PDF_PATH = "../website/canada/pdf/";
+	public final static List<String> EXCLUDE = Arrays.asList(new String[] { ".", "..", "pdf", "js", "css", "img" });
+	public final static String COMMAND = "C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe --print-media-type";
+	public static boolean REGERATE_ALL = false;
+
+	public static void main(String[] args) {
+		if (args.length == 1) {
+			if (args[0].equals("--generate-all")) {
+				REGERATE_ALL = true;
+			} else if (args[0].equals("--help")) {
+				System.out.println("Usage: java -classpath . -jar nlcgenpdf.jar");
+				System.out.println("Usage: java -classpath . -jar nlcgenpdf.jar --generate-all");
+				return;
+			}
+		}
+		File folder = new File(PAGES_PATH);
+		if (!folder.isDirectory()) {
+			throw new RuntimeException("Bad path (" + PAGES_PATH + ").");
+		}
+		try {
+			Files.list(Paths.get(PAGES_PATH)).filter(Files::isDirectory)
+					.filter(f -> !EXCLUDE.contains(f.getFileName().toString())).forEach(Main::generatePDF);
+		} catch (IOException e) {
+			throw new RuntimeException("IO Exception occured. ", e);
+		}
+	}
+
+	private static void generatePDF(Path file) {
+		// System.out.println("generatePDF: " + file);
+		String title = getTitle(file);
+		String pdfFileName = PDF_PATH + title + ".pdf";
+		if (!REGERATE_ALL && Files.exists(new File(pdfFileName).toPath())) {
+			System.out.println("Skipping PDF file : " + title + ".pdf");
+			return;
+		}
+		try {
+			System.out.println("Generating PDF file : " + title + ".pdf ...");
+			String command = COMMAND + " file:///"
+					+ file.resolve("index.html").toAbsolutePath().normalize().toString().replace("\\", "/") + " \""
+					+ pdfFileName + "\"";
+			// System.out.println("command : " + command);
+			Runtime.getRuntime().exec(command);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not generate PDF file : " + title + ".pdf", e);
+		}
+	}
+
+	private static String getTitle(Path file) {
+		Path index = file.resolve("index.html");
+		try {
+			List<String> title = Files.lines(index)
+					.filter(line -> line.contains("<title>") && line.contains("</title>")).map(line -> {
+						int titlePosition = line.indexOf("<title>");
+						int endTitlePosition = line.indexOf("</title>");
+						return line.substring(titlePosition + 7, endTitlePosition).trim();
+					}).collect(Collectors.toList());
+			if (title.isEmpty()) {
+				throw new RuntimeException("Could not get PDF title from html page : no title found. ");
+			}
+			return title.get(0);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not get PDF title from html page. ", e);
+		}
+	}
+}
---
+++ deployment/src/NewslettersCanadaTemplatesApply/src/org/pjb/newsletterscanada/templates/Main.java
@@ -0,0 +1,144 @@
+package org.pjb.newsletterscanada.templates;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public final static String PAGES_PATH = "../website/canada/";
+
+	public final static List<String> EXCLUDE = Arrays.asList(new String[] { ".", "..", "pdf", "js", "css", "img" });
+
+	public final static String FOR_EACH_NEWSLETTER_START = "${forEachNewsletter('start')}";
+	public final static String FOR_EACH_NEWSLETTER_END = "${forEachNewsletter('end')}";
+	public final static String N = "${N}";
+	public final static String DATE = "${DATE}";
+	public final static String TITLE = "${TITLE}";
+
+	private final static Map<String, String> title = new HashMap<String, String>();
+	private final static Map<String, String> date = new HashMap<String, String>();
+
+	public static void main(String[] args) {
+		File folder = new File(PAGES_PATH);
+		if (!folder.isDirectory()) {
+			throw new RuntimeException("Bad path (" + PAGES_PATH + ").");
+		}
+		fetchAllData();
+		applyTemplates();
+	}
+
+	private static void fetchAllData() {
+		System.out.println("Fetching data from pages...");
+		try {
+			Files.list(Paths.get(PAGES_PATH)).filter(Files::isDirectory)
+					.filter(f -> !EXCLUDE.contains(f.getFileName().toString())).forEach(Main::fetchData);
+		} catch (IOException e) {
+			throw new RuntimeException("IO Exception occured. ", e);
+		}
+		System.out.println("All data fetched !");
+	}
+
+	private static void fetchData(Path file) {
+		String n = file.getFileName().toString();
+		title.put(n, getTitle(file));
+		date.put(n, getDate(file));
+		System.out.println(
+				"   - Newsletter #" + n + " -> " + "title : \"" + title.get(n) + "\", date : \"" + date.get(n) + "\"");
+	}
+
+	private static void applyTemplates() {
+		System.out.println();
+		System.out.println("Applying templates...");
+		try {
+			Files.list(Paths.get(PAGES_PATH)).filter(Files::isRegularFile)
+					.filter(f -> f.getFileName().toString().endsWith(".template")).forEach(Main::applyTemplate);
+		} catch (IOException e) {
+			throw new RuntimeException("IO Exception occured. ", e);
+		}
+		System.out.println("Applied all templates !");
+	}
+
+	private static void applyTemplate(Path template) {
+		String destination = template.toString().substring(0, template.toString().length() - ".template".length());
+		System.out.println("   - Apply template : " + template + " -> " + destination);
+
+		try {
+			String templateContent = new String(Files.readAllBytes(template));
+			
+			do {
+				int boucle_start = templateContent.indexOf(FOR_EACH_NEWSLETTER_START);
+				int boucle_end = templateContent.indexOf(FOR_EACH_NEWSLETTER_END);
+	
+				if (boucle_start == -1 || boucle_end == -1) {
+					break;
+				}
+	
+				String toEval = templateContent.substring(boucle_start + FOR_EACH_NEWSLETTER_START.length(), boucle_end);
+				String compiled = "";
+	
+				for (String n : title.keySet()) {
+					String t = title.get(n), d = date.get(n);
+					compiled += toEval.replace(N, n).replace(TITLE, t).replace(DATE, d);
+				}
+	
+				templateContent = compiled = templateContent.substring(0, boucle_start) + compiled
+						+ templateContent.substring(boucle_end + FOR_EACH_NEWSLETTER_END.length());
+	
+			} while(templateContent.indexOf(FOR_EACH_NEWSLETTER_START) != -1);
+			
+			List<String> lines = new ArrayList<String>();
+			lines.add(templateContent);
+			Files.write(new File(destination).toPath(), lines, Charset.forName("UTF-8"));
+
+		} catch (IOException e) {
+			throw new RuntimeException("IO Exception occured. ", e);
+		}
+	}
+
+	private static String getTitle(Path file) {
+		Path index = file.resolve("index.html");
+		try {
+			List<String> title = Files.lines(index)
+					.filter(line -> line.contains("<h1>") && line.contains("<span class=\"title-date\">")).map(line -> {
+						int titlePosition = line.indexOf("<h1>");
+						int endTitlePosition = line.indexOf("<span class=\"title-date\">");
+						return line.substring(titlePosition + 4, endTitlePosition).trim();
+					}).collect(Collectors.toList());
+			if (title.isEmpty()) {
+				throw new RuntimeException("Could not get title from html page : no title found. ");
+			}
+			return title.get(0);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not get title from html page. ", e);
+		}
+	}
+
+	private static String getDate(Path file) {
+		Path index = file.resolve("index.html");
+		try {
+			List<String> title = Files.lines(index)
+					.filter(line -> line.contains("<span class=\"title-date\">") && line.contains("</span>"))
+					.map(line -> {
+						int titlePosition = line.indexOf("<span class=\"title-date\">");
+						int endTitlePosition = line.indexOf("</span>");
+						return line.substring(titlePosition + 25, endTitlePosition).trim();
+					}).collect(Collectors.toList());
+			if (title.isEmpty()) {
+				throw new RuntimeException("Could not get date from html page : no date found. ");
+			}
+			return title.get(0);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not get date from html page. ", e);
+		}
+	}
+}
